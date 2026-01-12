f66fcd0dad71b75ea98bceea3d514508be10e382
rajiv <rajivpaliath6119@gmail.com>
2026-01-11 20:31:52 -0500

    add JS fundamentals -part-1 challenges 1 and 2

---
+++ fundamentals-part-1/challenges/challenge-2.js
@@ -0,0 +1,42 @@
+/* CHALLENGE #2
+
+Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
+
+1. Print a nice output to the console, telling the user who has the higher BMI. The message can be either:
+
+"Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!".
+
+2. Modify the outputs above to use template literals to include the BMI values in the outputs.
+
+Example: "Mark's BMI (28.3) is higher than John's (23.9)!" or "John's BMI (29.1) is higher than Mark's (27)!".
+
+Note: Don't round the BMI values. Leave them as they are.
+
+
+
+👋 OPTIONAL: You can watch my solution in video format in the next lecture
+
+
+
+IMPORTANT: The ** operator is not supported in this editor. Please make sure to use exactly this formula mass / (height * height), and not this one mass / (height ** 2).
+
+*/
+
+const massMark  =  78;
+const heightMark = 1.69;
+const  massJohn = 92;
+const heightJohn =  1.76;
+
+const BMIMark  =  massMark / (heightMark * heightMark);
+const BMIJohn  = (massJohn /(heightJohn * heightJohn));
+
+console.log(BMIMark, BMIJohn)
+
+ const  markHigherBMI  =  BMIMark > BMIJohn;
+
+ if (markHigherBMI) {
+ console.log( `Mark's BMI (${BMIMark}) is higher than John's BMI(${BMIJohn})`);
+ }
+ else  {
+    console.log(`Mark's BMI (${BMIMark}) is lesser  than John's BMI(${BMIJohn})`)
+ }
